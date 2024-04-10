52655071ada131c0f50b2974ac62bb4ba7950668
JonathanRizocki <j.rizocki@gmail.com>
2024-04-10 13:23:06 -0700

    Currency code & tests.

--- money/errors.go
+++ money/errors.go
@@ -15,4 +15,7 @@ const (
 	// ErrTooLarge is returned if the quantity is too large.
 	// This would cause floating point precision errors.
 	ErrTooLarge = Error("quantity over 10^12 is too large")
+
+	// ErrInvalidCurrencyCode is returned when the currency to parse is not a standard 3-letter code.
+	ErrInvalidCurrencyCode = Error("invalid currency code")
 )
