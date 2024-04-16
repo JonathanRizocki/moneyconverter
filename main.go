a9dc266dc9d3d37d279949cdc21a5a1e70fc61fe
JonathanRizocki <j.rizocki@gmail.com>
2024-04-16 14:33:59 -0700

    Setup CLI flags.

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"learngo-pockets/moneyconverter/money"
+	"os"
+)
+
 func main() {
+
+	from := flag.String("from", "", "source currency, required")
+	to := flag.String("to", "EUR", "target currency")
+
+	flag.Parse()
+
+	value := flag.Arg(0)
+	if value == "" {
+		_, _ = fmt.Fprintln(os.Stderr, "missing amount to convert")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fmt.Println(*from, *to, value)
+
+	fromCurrency, err := money.ParseCurrency(*from)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to parse source currency %q: %s.\n", *from, err.Error())
+		os.Exit(1)
+	}
+
+	toCurrency, err := money.ParseCurrency(*to)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to parse source currency %q: %s.\n", *from, err.Error())
+		os.Exit(1)
+	}
+
+	quantity, err := money.ParseDecimal(value)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to parse value %q: %s.\n", value, err.Error())
+		os.Exit(1)
+	}
+
+	amount, err := money.NewAmount(quantity, fromCurrency)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("Amount:", amount, "; Currency:", toCurrency)
 
 }
